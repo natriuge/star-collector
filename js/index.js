170f278b5f9fe5e9d4e4049a4886a87fccae97df
“Natalia <natalia.rudiger@gmail.com>
2022-02-15 22:51:04 -0200

    created character and elements sprites

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const img = new Image();
+img.src = 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/146636240/original/e14b75037ee60d82542dd75e84104f2d05eb82a4/make-backgrounds-for-games.png';
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const backgroundImage = {
+  img: img
+}
+
+
+
+
+
+window.onload = () => {
+  var canvas = document.getElementById('canvas');
+
+};
