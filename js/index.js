c4cc9c2f681fc53ac88aab113253c8cea41e1003
“Natalia <natalia.rudiger@gmail.com>
2022-02-16 02:33:13 -0200

    Image() constructor and classes

--- js/index.js
+++ js/index.js
@@ -21,14 +21,57 @@ superStarImg.src = '/images/super star.png';
 const meteorImg = new Image();
 meteorImg.src = '/images/meteor.png';
 
+// Array com os elementos, tamanhos ainda pendentes
+const elements = [
+  {img:starImg, width:300, heigth: 300},
+  {img:superStarImg, width:100, heigth: 100}, 
+  {img:meteorImg, width:150, heigth: 150},
+]
+
+
+// Classe do personagem
+class characterPlayer {
+  // Recebe respectivamente:
+  constructor(x, y, width, height, img) {
+    this.x = x; // Coordenada x
+    this.y = y; // Coordenada y
+    this.width = width; // Largura do personagem
+    this.height = height; // Altura do personagem
+    this.img = img; // Imagem do personagem
+    this.speedX = 0; // Velocidade no eixo x
+    this.speedY = 0; // Velocidade no eixo y
+    this.starsCounter = 0; // Inicia com 0 estrelas
+  }
+}
+
+
+// Classe do jogo
+class Game {
+  // Recebe respectivamente:
+  constructor(background, player) {
+    this.background = background; // Imagem de fundo
+    this.player = player; // Imagem do personagem
+    this.elements = []; // Array de elementos
+    this.frames = 0; 
+    this.score = 0; // Contagem de estrelas
+    this.animationId; 
+  }
+}
 
 
 
 
+
+
+// Evento Listener que atualiza a página:
 window.addEventListener("load", () => {
     
-  // Evento Listener que atualiza a página:
-  // Recebe: Imagem de fundo, posição x, posição y, largura e altura. // 
+  // Recebe respectivamente:
+  // 01. Imagem de fundo
+  // 02. Coordenada x
+  // 03. Coordenada y
+  // 04. Largura
+  // 05. Altura
   ctx.drawImage(bgImgCanvas, 0, 0, 1000, 700) 
 
 })
